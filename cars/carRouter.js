362ad9f1ce0c7f89f7f2cea26fa82490b7aa759b
BuildTools <arthurpisakhov1@gmail.com>
2019-10-25 22:17:23 -0400

    POST and middleware

--- cars/carRouter.js
+++ cars/carRouter.js
@@ -8,4 +8,18 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json(err))
 })
 
+router.post('/', validateInput, (req, res) => {
+  db('cars').insert(req.body)
+    .then(cars => res.status(201).json(cars))
+    .catch(err => res.status(500).json(err))
+})
+
+function validateInput(req, res, next) {
+  const { car, VIN, make, model, mileage } = req.body
+
+  if (!req.body) res.status(400).json({ error: 'No inputs sent' })
+  else if (!car || !VIN || !make || !model || !mileage ) res.status(400).json({ error: 'Missing some inputs: car/VIN/make/model/mileage' })
+  else next();
+}
+
 module.exports = router;
